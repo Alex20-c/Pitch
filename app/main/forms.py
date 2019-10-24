2ab8cec5d27d5792da0a46635cd01e47b2bce3c6
Alex Kiprop <kipropalex59@gmail.com>
2019-10-24 09:47:14 +0300

    create a class form to comment on a pitch

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
 
@@ -7,19 +7,21 @@ class CategoryForm(FlaskForm):
     """
     class to create a form to create category
     """
-    name =  SubmitField('Pitch category',validators=[Required()])
-    sybmit = SubmitField('Create')
+    name = StringField('Pitch Category',validators=[Required()])
+    submit = SubmitField('Create')
+
 
 class PitchForm(FlaskForm):
     """
     class to create form to write pitch
     """
-    pitch = SubmitField('Pitch Content', validators=[Required()])
+    pitch = StringField('Pitch Content', validators=[Required()])
     submit = SubmitField('Submit')
-    
+
+
 class CommentForm(FlaskForm):
     """
     class to create form to comment on a pitch
     """
     comment = StringField('Comment Content', validators=[Required()])
-    submit =  SubmitField('Submit')
+    submit = SubmitField('Submit')
