7c03f81a28b78d64b560d9ba27a2e507d95cd14f
Alex Kiprop <kipropalex59@gmail.com>
2019-10-24 18:23:56 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -5,16 +5,26 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATA_URI')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATA_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL.PASSWORD')
+    DATABASE_PASS = os.environ.get('DATABASE_PASS')
+            
+    # emails configuration
+    
+    MAIL_SERVER=os.environ.get('MAIL_SERVER')
 
-   
+    MAIL_PORT=os.environ.get('MAIL_PORT')
+
+    MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS')
+
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+
+    DEBUG = True      
+  
+    
     @staticmethod
 
     def init_app(app):
@@ -29,8 +39,17 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     # SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql+psycopg2://alex:aleki@localhost/pitches')
-    # pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
+    
+class DevConfig(Config):
+    '''
+    Development configuration child class
 
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alex:aleki@localhost/pitches'
 
 class TestConfig(Config):
     '''
@@ -41,16 +60,7 @@ class TestConfig(Config):
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alex:aleki@localhost/pitches'
     
-    DEBUG = True
-
-class DevConfig(Config):
-    '''
-    Development configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
+  
 
 
 config_options = {
